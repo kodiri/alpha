1abdc90c11f517a4940585331991e1ebae4259b6
“ClarissaWaldron” <clarissa509@hotmail.com>
2019-12-09 17:15:11 +0000

    preview site componentised

--- src/PreviewSite/PreviewSite.js
+++ src/PreviewSite/PreviewSite.js
@@ -2,9 +2,6 @@ import React from 'react';
 import './PreviewSite.css'
 import NavBar from './NavBar/NavBar'
 import FestivalInfo from './FestivalInfo/FestivalInfo'
-// import hogsozzle1 from '../images/hogsozzle-img.jpg';
-// import hogsozzle from '../../images/Logos/hogsozzle.png';
-// import FAQ from '../../images/Logos/faq.png';
 import Faq from '../images/faq.jpg'
 import Tickets from './Tickets/Tickets'
 import Location from './Location/Location'
