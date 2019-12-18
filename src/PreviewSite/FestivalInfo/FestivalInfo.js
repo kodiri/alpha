b9c22e7cd1a6b3ede3ea0e4824130af112e08840
Zara <48896844+zarbar@users.noreply.github.com>
2019-12-18 12:34:24 +0000

    ironing out conflicts

--- src/PreviewSite/FestivalInfo/FestivalInfo.js
+++ src/PreviewSite/FestivalInfo/FestivalInfo.js
@@ -2,19 +2,22 @@ import React from 'react'
 import './FestivalInfo.css'
 
 export default class FestivalInfo extends React.Component {
-    constructor (props) {
-        super (props) 
-        this.state = {}
+    constructor(props) {
+        super(props)
+        this.state = {
+            festivalName: 'Hogsozzle Festival'
+        }
     }
-    render () {
+    render() {
         return (
-        <>
-         <h1>Hogsozzle Festival</h1>
-            <div>
-                <p>Performing arts and music festival<br/> Bulgaria <br/>
-                June 4-7 2020
+            <>
+                <h1>{this.state.festivalName}</h1>
+                <div>
+                    <p>Performing arts and music festival<br /> Bulgaria <br />
+                        June 4-7 2020
                 </p>
-            </div>
-        </>
-        )};
+                </div>
+            </>
+        )
+    };
 }
