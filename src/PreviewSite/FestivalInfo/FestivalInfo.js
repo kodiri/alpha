75bf2636023b7d20d9997583d4bb9cf0542df189
Zara <48896844+zarbar@users.noreply.github.com>
2019-12-18 14:48:20 +0000

    added states to festivalInfo class component

--- src/PreviewSite/FestivalInfo/FestivalInfo.js
+++ src/PreviewSite/FestivalInfo/FestivalInfo.js
@@ -5,18 +5,22 @@ export default class FestivalInfo extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            festivalName: 'Hogsozzle Festival'
+            festivalName: 'Hogsozzle Festival',
+            blurb: 'Performing arts and music festival',
+            location: 'Sofia, Bulgaria',
+            eventDescription: '',
+            startDate: '04 Jun 2021',
+            endDate: '07 Jun 2021'
+
         }
     }
     render() {
         return (
             <>
                 <h1>{this.state.festivalName}</h1>
-                <div>
-                    <p>Performing arts and music festival<br /> Bulgaria <br />
-                        June 4-7 2020
-                </p>
-                </div>
+                <p>{this.state.blurb}</p>
+                <p>{this.state.location}</p>
+                <p>{this.state.startDate} - {this.state.endDate}</p>
             </>
         )
     };
