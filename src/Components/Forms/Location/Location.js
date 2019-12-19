36f1c578f43c835d79c3d04fbd3070ceb93bde5f
Zara <48896844+zarbar@users.noreply.github.com>
2019-12-19 20:31:01 +0000

    css for location form

--- src/Components/Forms/Location/Location.js
+++ src/Components/Forms/Location/Location.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './location.css';
 
 export default function Location() {
 
@@ -12,17 +13,20 @@ export default function Location() {
 
     return (
         <div className='locationFormPage'>
-            <label>Add full event location: </label>
-            <input type="text" onChange={handlePostcodeChange} placeholder='Address Line 1' />
-            <input type="text" onChange={handlePostcodeChange}
-                placeholder='Address Line 2' />
-            <input type="text" onChange={handlePostcodeChange} placeholder='Address Line 3' />
-            <label>Add details about getting to your venue: <br /></label>
-            <textarea type="text" onChange={accessDetails} placeholder="Eg. nearest aiports, train stations, taxis, parking, shuttle buses etc" />
-            <div className="containerLocation">
-                <a href='/dashboard/123' className="item">Save and return to dashboard</a>
-                <a href='/faqForm' className="item">Save and continue</a>
-            </div>
+            <h1>Step 3: Location</h1>
+            <form id='locationInfoForm'>
+                <p>Add full event location: </p>
+                <input className='addressLineInput' type="text" onChange={handlePostcodeChange} placeholder='Address Line 1' />
+                <input className='addressLineInput' type="text" onChange={handlePostcodeChange}
+                    placeholder='Address Line 2' />
+                <input className='addressLineInput' type="text" onChange={handlePostcodeChange} placeholder='Address Line 3' />
+                <p>Add details about getting to your venue: <br /></p>
+                <textarea className="infoTextArea" type="text" onChange={accessDetails} placeholder="Eg. nearest aiports, train stations, taxis, parking, shuttle buses etc" />
+                <div className="containerLocation">
+                    <a href='/dashboard/123' className="item">Save and return to dashboard</a>
+                    <a href='/faqForm' className="item">Save and continue</a>
+                </div>
+            </form>
         </div>
     )
 }
