c85967b173dd3dfd899d1535edabb51d758dcf42
Zara <48896844+zarbar@users.noreply.github.com>
2019-12-19 22:07:24 +0000

    bugs gone

--- src/Components/Forms/Location/Location.js
+++ src/Components/Forms/Location/Location.js
@@ -3,8 +3,14 @@ import './location.css';
 
 export default function Location() {
 
-    function handlePostcodeChange(e) {
-        localStorage.setItem("postcode", e.target.value)
+    function handleAddressLineOne(e) {
+        localStorage.setItem("Address Line 1", e.target.value)
+    }
+    function handleAddressLineTwo(e) {
+        localStorage.setItem("Address Line 2", e.target.value)
+    }
+    function handleAddressLineThree(e) {
+        localStorage.setItem("Address Line 3", e.target.value)
     }
 
     function accessDetails(e) {
@@ -16,10 +22,14 @@ export default function Location() {
             <h1>Step 3: Location</h1>
             <form id='locationInfoForm'>
                 <p>Add full event location: </p>
-                <input className='addressLineInput' type="text" onChange={handlePostcodeChange} placeholder='Address Line 1' />
-                <input className='addressLineInput' type="text" onChange={handlePostcodeChange}
+
+                <input className='addressLineInput' type="text" onChange={handleAddressLineOne} placeholder='Address Line 1' />
+
+                <input className='addressLineInput' type="text" onChange={handleAddressLineTwo}
                     placeholder='Address Line 2' />
-                <input className='addressLineInput' type="text" onChange={handlePostcodeChange} placeholder='Address Line 3' />
+
+                <input className='addressLineInput' type="text" onChange={handleAddressLineThree} placeholder='Address Line 3' />
+
                 <p>Add details about getting to your venue: <br /></p>
                 <textarea className="infoTextArea" type="text" onChange={accessDetails} placeholder="Eg. nearest aiports, train stations, taxis, parking, shuttle buses etc" />
                 <div className="containerLocation">
