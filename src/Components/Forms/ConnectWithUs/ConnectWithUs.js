474f7451538697edef7de18e594956ab33e4ecd8
Zara <48896844+zarbar@users.noreply.github.com>
2019-12-19 21:53:18 +0000

    fixing errors

--- src/Components/Forms/ConnectWithUs/ConnectWithUs.js
+++ src/Components/Forms/ConnectWithUs/ConnectWithUs.js
@@ -5,12 +5,7 @@ export default function ContactPage() {
     function socialMedia1Change(e) {
         localStorage.setItem('Social Media 1', e.target.value)
     }
-    function socialMedia2Change(e) {
-        localStorage.setItem('Social Media 2', e.target.value)
-    }
-    function socialMedia3Change(e) {
-        localStorage.setItem('Social Media 3', e.target.value)
-    }
+
     function emailChange(e) {
         localStorage.setItem('Email', e.target.value)
     }
@@ -22,10 +17,9 @@ export default function ContactPage() {
         <>
             <h1>Step 5 - Connect with us</h1>
             <form className='connectForm'>
-                <p>Add social media links:</p>
+                <p>Social media link</p>
                 <textarea className='infoTextArea' type="text" id='connect' onChange={socialMedia1Change}></textarea>
-                <textarea className='infoTextArea' type="text" id='connect' onChange={socialMedia2Change}></textarea>
-                <textarea className='infoTextArea' type="text" id='connect' onChange={socialMedia3Change}></textarea>
+
                 <p>Add contact email </p>
                 <textarea className='infoTextArea' type="email" id='connect' onChange={emailChange}></textarea>
                 <p>Add company address</p>
