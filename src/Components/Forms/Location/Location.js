3656fb2dbc05f1aeaeefd6492e022a2c6e6a9516
Zara <48896844+zarbar@users.noreply.github.com>
2019-12-19 20:15:56 +0000

    added address lines to location

--- src/Components/Forms/Location/Location.js
+++ src/Components/Forms/Location/Location.js
@@ -1,27 +1,28 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 export default function Location() {
-    let [postcode, setPostcode] = useState("");
+
     function handlePostcodeChange(e) {
-        setPostcode(e.target.value);
         localStorage.setItem("postcode", e.target.value)
     }
+
     function accessDetails(e) {
         localStorage.setItem("Access", e.target.value)
     }
 
-
     return (
-        <>
-            <label>Add postcode of your event: </label><input type="text" onChange={handlePostcodeChange}/>
-            <label>Add details about getting to your venue: </label><input type="text" onChange={accessDetails}/>
-            <textarea placeholder="Eg. nearest aiports, train stations, taxis, parking, shuttle buses etc"></textarea>
-      />
-      <div className="containerLocation">
-                        <a href='/dashboard/123' className="item">Save and return to dashboard</a>
-                        <a href='/faqForm' className="item">Save and continue</a>
-                    </div>
-
-        </>
+        <div className='locationFormPage'>
+            <label>Add full event location: </label>
+            <input type="text" onChange={handlePostcodeChange} placeholder='Address Line 1' />
+            <input type="text" onChange={handlePostcodeChange}
+                placeholder='Address Line 2' />
+            <input type="text" onChange={handlePostcodeChange} placeholder='Address Line 3' />
+            <label>Add details about getting to your venue: <br /></label>
+            <textarea type="text" onChange={accessDetails} placeholder="Eg. nearest aiports, train stations, taxis, parking, shuttle buses etc" />
+            <div className="containerLocation">
+                <a href='/dashboard/123' className="item">Save and return to dashboard</a>
+                <a href='/faqForm' className="item">Save and continue</a>
+            </div>
+        </div>
     )
 }
